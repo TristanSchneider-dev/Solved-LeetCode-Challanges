a7deeaebdec6d6dbae126b50254409b39b6b88a5
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-15 12:54:21 +0200

    Aufgabe 3 gelöst

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,24 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #all possible substring approach
-        charsOfString = []
-        for char in s:
-            if char in charsOfString: continue
-            charsOfString.append(char)
+        seqList = []
+        for index, tail in enumerate(s):
+            seqString = ""
+            for head in s[index:]:
+                if head not in seqString: seqString += head
+                else: break
+            seqList.append(seqString)
 
-        #algorithmic approach
-        possibleCombos = []
-        currentString = ""
-        for tail in s:
-            firstIteration = False
-            for head in s:
-                if head == tail and firstIteration:
-                    break
-                currentString += head
-                firstIteration = True
-            possibleCombos.append(currentString)
-            currentString = ""
-        print(possibleCombos)
-        #"abcabcbb"
-        #['abc', 'a', 'ab', 'abc', 'a', 'ab', 'a', 'a']
-        
+        maxLen = 0
+        for i in seqList:
+            if len(i) > maxLen: maxLen = len(i)
+        return maxLen
