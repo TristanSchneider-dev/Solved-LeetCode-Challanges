aefabb8c6ddc6c4a1fa7571065d192120792234c
pete-win <tristan.peter.schneider@gmail.com>
2024-10-22 15:57:33 +0200

    Aufgabe 12 begonnen

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,7 +1,29 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        romanValue = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-
+        romanValue = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+        
+        romanString = ''
         while num > 0:
-            num = num / max()
-                
+            if num > 1000:
+                romanString += 'M'
+                num -= 1000
+            elif num > 500:
+                romanString += 'D'
+                num -= 500
+            elif num > 100:
+                romanString += 'C'
+                num -= 100
+            elif num > 50:
+                romanString += 'L'
+                num -= 50
+            elif num > 10:
+                romanString += 'X'
+                num -= 10
+            elif num > 5:
+                romanString += 'V'
+                num -= 5
+            else:
+                romanString += 'I'
+                num -= 1
+
+        return romanString
