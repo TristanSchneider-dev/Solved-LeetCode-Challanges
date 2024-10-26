6808e084f87057216041b9c7b9b0c6929098b988
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-26 19:44:01 +0200

    Aufgabe 15 begonnen (Laufzeit muss optimiert werden)

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,8 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        for index, x in enumerate(nums):
-            tail = index + 1
-            head = len(nums) - 1
-            while head > tail:
-                if x + nums[head] + nums[tail] == 0:
-                    print(index, tail, head)
+        sumEqualsZero = []
+        nums.sort()
+        
+        for x in range(len(nums)):
+            if x > 0 and nums[x] == nums[x - 1]:
+                continue
+            for y in range(x + 1, len(nums)):
+                if y > x + 1 and nums[y] == nums[y - 1]:
+                    continue
+                for z in range(y + 1, len(nums)):
+                    if z > y + 1 and nums[z] == nums[z - 1]:
+                        continue
+                    if nums[x] + nums[y] + nums[z] == 0:
+                        sumEqualsZero.append([nums[x], nums[y], nums[z]])
+        
+        return sumEqualsZero
