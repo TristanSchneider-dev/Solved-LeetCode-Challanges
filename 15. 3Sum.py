45c303622c38dcf0a71895e4e1d42737482fa973
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-26 16:48:11 +0200

    Aufgabe 15 begonnen

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,3 +1,8 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-	
+        for index, x in enumerate(nums):
+            tail = index + 1
+            head = len(nums) - 1
+            while head > tail:
+                if x + nums[head] + nums[tail] == 0:
+                    print(index, tail, head)
