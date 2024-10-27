b08b014f10d416424934ecbb02a24d5ad3dee414
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-27 12:33:51 +0100

    Aufgabe 15 gelöst

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,18 +1,36 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sumEqualsZero = []
         nums.sort()
-        
-        for x in range(len(nums)):
-            if x > 0 and nums[x] == nums[x - 1]:
-                continue
-            for y in range(x + 1, len(nums)):
-                if y > x + 1 and nums[y] == nums[y - 1]:
-                    continue
-                for z in range(y + 1, len(nums)):
-                    if z > y + 1 and nums[z] == nums[z - 1]:
-                        continue
-                    if nums[x] + nums[y] + nums[z] == 0:
-                        sumEqualsZero.append([nums[x], nums[y], nums[z]])
-        
-        return sumEqualsZero
+        threeSums = []
+
+        for index, x in enumerate(nums):
+            if index > 0 and nums[index] == nums[index - 1]:
+                continue  # Überspringe gleiche Werte für x
+
+            target = -x
+            tail = index + 1
+            head = len(nums) - 1
+            
+            while tail < head:
+                current_sum = nums[tail] + nums[head]
+
+                if current_sum == target:
+                    threeSums.append([x, nums[tail], nums[head]])
+
+                    # Überspringe alle gleichen Werte für tail und head
+                    while tail < head and nums[tail] == nums[tail + 1]:
+                        tail += 1
+                    while tail < head and nums[head] == nums[head - 1]:
+                        head -= 1
+
+                    tail += 1
+                    head -= 1
+
+                elif current_sum < target:
+                    tail += 1
+                else:
+                    head -= 1
+
+        return threeSums
