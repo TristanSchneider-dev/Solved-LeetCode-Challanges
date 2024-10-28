16dcb4227487e5514dd6f8d27147a5f4b2c23718
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-28 16:36:13 +0100

    Aufgabe 17 begonnen

--- 17. Letter Combinations of a Phone Number.cs
+++ 17. Letter Combinations of a Phone Number.cs
@@ -1,5 +1,28 @@
+using System;
+using System.Collections.Generic;
+
 public class Solution {
     public IList<string> LetterCombinations(string digits) {
+        if (string.IsNullOrEmpty(digits)) {
+            return new List<string>();
+        }
+
+        Dictionary<int, string> phoneMap = new Dictionary<int, string> {
+            [2] = "abc",
+            [3] = "def",
+            [4] = "ghi",
+            [5] = "jkl",
+            [6] = "mno",
+            [7] = "pqrs",
+            [8] = "tuv",
+            [9] = "wxyz"
+        };
+
+        List<string> result = new List<string>();
         
+        foreach()
+
+        return result;
     }
+
 }
