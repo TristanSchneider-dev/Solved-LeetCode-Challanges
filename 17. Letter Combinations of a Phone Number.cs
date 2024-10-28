bcf18e173634c2be01291854eda6d2a5505c2818
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-28 17:33:33 +0100

    Aufgabe 17 gelöst

--- 17. Letter Combinations of a Phone Number.cs
+++ 17. Letter Combinations of a Phone Number.cs
@@ -7,22 +7,37 @@ public class Solution {
             return new List<string>();
         }
 
-        Dictionary<int, string> phoneMap = new Dictionary<int, string> {
-            [2] = "abc",
-            [3] = "def",
-            [4] = "ghi",
-            [5] = "jkl",
-            [6] = "mno",
-            [7] = "pqrs",
-            [8] = "tuv",
-            [9] = "wxyz"
+        Dictionary<char, string> phoneMap = new Dictionary<char, string> {
+            ['2'] = "abc",
+            ['3'] = "def",
+            ['4'] = "ghi",
+            ['5'] = "jkl",
+            ['6'] = "mno",
+            ['7'] = "pqrs",
+            ['8'] = "tuv",
+            ['9'] = "wxyz"
         };
 
-        List<string> result = new List<string>();
+        List<string> letters = new List<string>();
         
-        foreach()
+        foreach(char digit in digits){
+            letters.Add(phoneMap[digit]);
+        }
 
+        List<string> result = new List<string>();
+        genCombos(letters, 0, "", result);
+        
         return result;
     }
 
+    public void genCombos(List<string> letters, int index, string current, List<string> result){
+        if (index == letters.Count){ //Rekursionsanker
+            result.Add(current);
+            return;
+        }
+
+        foreach(char letter in letters[index]){
+            genCombos(letters, index + 1, current + letter, result);
+        }
+    }
 }
