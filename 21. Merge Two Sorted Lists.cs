f29d04b5bcf9afc2e4918aa0fc932f36400c22bf
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-10-31 20:11:50 +0100

    Aufgabe 21 gelöst

--- 21. Merge Two Sorted Lists.cs
+++ 21. Merge Two Sorted Lists.cs
@@ -9,8 +9,32 @@
  *     }
  * }
  */
+
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        
+
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val <= list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return head.next;
     }
 }
