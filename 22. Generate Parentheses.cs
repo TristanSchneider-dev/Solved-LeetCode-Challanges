366f61a65fc0913643a7fe36354a09b482371356
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-11-02 14:50:49 +0100

    Aufgabe 22 gelöst

--- 22. Generate Parentheses.cs
+++ 22. Generate Parentheses.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public IList<string> GenerateParenthesis(int n) {
-        
+        IList<string> validParentheses = new List<string>();
+
+        void Generate(string s, int open, int closed) {
+            if (s.Length == 2 * n) {
+                validParentheses.Add(s);
+                return;
+            }
+
+            if (open < n) Generate(s + "(", open + 1, closed);
+
+            if (closed < open) Generate(s + ")", open, closed + 1);
+        }
+
+        // Anfang der Rekursion
+        Generate("", 0, 0);
+
+        return validParentheses;
     }
 }
