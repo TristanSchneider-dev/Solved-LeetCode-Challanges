7b6c90ca26929b68a91c4671a0144082122fc63c
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-11-04 18:37:54 +0100

    Aufgabe 23 begonnen

--- 23. Merge k Sorted Lists.cs
+++ 23. Merge k Sorted Lists.cs
@@ -9,8 +9,24 @@
  *     }
  * }
  */
+
 public class Solution {
     public ListNode MergeKLists(ListNode[] lists) {
-        
+        PriorityQueue<ListNode, int> heap = new PriorityQueue<ListNode, int>();
+
+        foreach(ListNode startNode in lists){
+            if (startNode != null) heap.Enqueue(startNode, startNode.val);
+            Console.WriteLine(startNode.val);
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+
+        /*
+        while(heap.Count > 0){
+            tail = heap.Dequeue();
+        }
+        */
+        return null;
     }
 }
