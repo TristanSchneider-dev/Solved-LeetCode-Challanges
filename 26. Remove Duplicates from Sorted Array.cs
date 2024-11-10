5478539c9f9194f5c7bf175f705161da9bfa63b2
TristanSchneider-dev <tristan.peter.schneider@gmail.com>
2024-11-10 14:19:38 +0100

    Aufgabe 26 begonnen

--- 26. Remove Duplicates from Sorted Array.cs
+++ 26. Remove Duplicates from Sorted Array.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
+        int counter = 0;
+
+        for (int i; i < nums.Length; i++){
+            int j = 1;
+            while (nums[i] == nums[i+j]){
+                j++;
+            }
+            nums[i+1] = nums[i+j+1];
+
+        }
         
+        return counter;
     }
 }
